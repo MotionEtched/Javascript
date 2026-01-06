634bc891eb86309efff947197577eaa4f6819b6b
MotionEtched <motionetched123@gmail.com>
2026-01-06 20:03:40 +0545

    Variables in JS

---
+++ variables.js
@@ -0,0 +1,42 @@
+// 1. Declare three variables and print them
+let name = "Ana";  
+let age = 21;           
+let isStudent = true;   
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+
+// 2. Create a variable and change its value
+let num = 10;            // initial value
+console.log("Initial value of num:", num);
+
+num = 25;                // updated value
+console.log("Updated value of num:", num);
+
+
+// 3. Swap values without using a third variable
+let a = 5;
+let b = 10;
+
+// Swapping logic using arithmetic operators
+a = a + b;
+b = a - b;
+a = a - b;
+
+console.log("After swapping:");
+console.log("a =", a);
+console.log("b =", b);
+
+
+// 4. Constant variable
+const PI = 3.14;
+
+// PI = 3.14159;  
+console.log("Value of PI:", PI);
+
+
+// 5. Favorite color
+let favoriteColor = "Black";
+console.log("My favorite color is", favoriteColor);
