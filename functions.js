aac5e47562eb470b4d5b3478bf813793c8ac4a9b
MotionEtched <motionetched123@gmail.com>
2026-01-12 22:16:12 +0545

    Question 1

---
+++ functions.js
@@ -0,0 +1,6 @@
+//Question 1. Write a function that takes a name and returns a greeting.
+function greetUser(name) {  //declares a function with parameter name
+  return "Hello, " + name + "! Welcome to JavaScript."; //sends the message back .."name" calls the function .."+" combines strings and variable
+}
+
+console.log(greetUser("John"));  //prints the name
