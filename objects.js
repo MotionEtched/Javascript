33a9e5442a4aed656fcb72a6baa5943f5a5b1509
MotionEtched <motionetched123@gmail.com>
2026-01-15 20:18:21 +0545

    Question 2,3

--- objects.js
+++ objects.js
@@ -6,3 +6,12 @@ const person = {
 };
 
 console.log(person);
+
+// Question 2. Access and print a specific property
+console.log(person.name);
+
+//Question 3. Add a new property & update an existing one
+person.country = "Nepal";   // add new property
+person.age = 21;            // update existing property
+
+console.log(person);
